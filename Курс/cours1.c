e314e44e7ecf36bcc29f830c3ffc030d90591e89
GulievIlya <113112989+GulievIlya@users.noreply.github.com>
2022-12-20 17:49:56 +0300

    Update cours1.c

--- Курс/cours1.c
+++ Курс/cours1.c
@@ -18,11 +18,11 @@ typedef struct str polz;
 
 int put_str(polz* zap, int chislo);
 int vivod(polz* zap, int chislo);
-int vivodstr(polz* zap, int *str);
-int poisk1(polz* zap, int chisl,int* poisk[znach]);
+int vivodstr(polz* zap, int i);
+int poisk1(polz* zap, int chisl);
 int poisk2(polz* zap, int chisl);
 int sorting(polz* zap, int chislo);
-int readfile(polz* ptr_struct, int size,FILE *ptr_file);
+int readfile(polz* ptr_struct, int size, FILE* ptr_file);
 void vvod(polz* ptr_struct, int size);
 
 int main() {
@@ -30,9 +30,7 @@ int main() {
 	setlocale(LC_ALL, "RU");
 	FILE* file_ptr;
 	int n, chislo = 0;//n-действие 
-	int *ptr_poisk = &poisk[chislo];
-	ptr_poisk = (int*)malloc(znach);
-	polz baze[znach], * ptr_zapis = &baze[chislo];
+	polz *ptr_zapis ;
 	FILE* ptr_file = fopen("BD.txt", "r");
 	ptr_zapis = (struct polz*)malloc(chislo * sizeof(polz));
 	while (1) {
@@ -47,11 +45,13 @@ int main() {
 			vivod(ptr_zapis, chislo);
 			break;
 		case 3:
-			j = poisk1(ptr_zapis, chislo,ptr_poisk);
-			//printf("ptr - %d %d ", ptr_poisk[0], ptr_poisk[1]);
+			j = poisk1(ptr_zapis, chislo);
+			if (j < -900) break;
+			vivodstr(ptr_zapis, j);
 			break;
 		case 4:
 			j1 = poisk2(ptr_zapis, chislo);
+			if (j1 < -200) break;
 			vivodstr(ptr_zapis, j1);
 			break;
 		case 5:
@@ -70,7 +70,7 @@ int main() {
 	}
 }
 int put_str(polz* zap, int chislo) {
-	if (chislo > znach) realloc(zap,znach,chislo);
+	if (chislo > znach) realloc(zap, znach*(chislo+1));
 	printf("\n1)Логин:");
 	scanf("%s", &zap[chislo].login);
 	printf("\n2)Дата входа:");
@@ -91,79 +91,72 @@ int put_str(polz* zap, int chislo) {
 	return chislo;
 }
 int vivod(polz* zap, int chislo) {
-	for (int i = 0;i < chislo;i++) {
-		printf("\n___________________________________________");
-		printf("\n|Логин пользователя(7сим):  %s\n", zap[i].login);
-		printf("|Дата выхода в on-line:  %d\n", zap[i].data_vh);
-		printf("|Время начала сеанса on-line:  %d:%d:%d\n", zap[i].online[0], zap[i].online[1], zap[i].online[2]);
-		printf("|Время завершения сеанса on-line:  %d:%d:%d\n", zap[i].ofline[0], zap[i].ofline[1], zap[i].ofline[2]);
-		printf("|скаченные/отправленные МБ:  %.2f/%.2f\t  \n", zap[i].dowloand, zap[i].send);
-		printf("|___________________________________________\n");
+	puts("______________________________________________________________________________________________________________________");
+	puts("|Номер  |Логин  |Дата выхода в on-line|Время начала сеанса on-line|Время завершения сеанса|Скачанные/отправленные М/Б|");
+	for (int i = 0; i < chislo; i++) {
+		printf("| %d\t| %s\t| %d\t              | %d:%d:%d\t                  | %d:%d:%d\t          | %.2f/%.2f\t             |\n",i, zap[i].login, zap[i].data_vh, zap[i].online[0], zap[i].online[1], zap[i].online[2], zap[i].ofline[0], zap[i].ofline[1], zap[i].ofline[2], zap[i].dowloand, zap[i].send);
 	}
+	printf("|____________________________________________________________________________________________________________________|");
 	return 0;
 }
-int vivodstr(polz* zap, int* str) {
-	//for (int i = 0;i < (sizeof(str) / 4);i++);
+int vivodstr(polz* zap, int i) {//zap[a].login
+	puts("______________________________________________________________________________________________________________________");
+	puts("|Номер  |Логин  |Дата выхода в on-line|Время начала сеанса on-line|Время завершения сеанса|Скачанные/отправленные М/Б|");
+	printf("| %d\t| %s\t| %d\t              | %d:%d:%d\t                  | %d:%d:%d\t          | %.2f/%.2f\t             |\n", i, zap[i].login, zap[i].data_vh, zap[i].online[0], zap[i].online[1], zap[i].online[2], zap[i].ofline[0], zap[i].ofline[1], zap[i].ofline[2], zap[i].dowloand, zap[i].send);
+	printf("|____________________________________________________________________________________________________________________|");
 }
-int poisk1(polz* zap, int chisl, int* poisk){
+int poisk1(polz* zap, int chisl) {
 	char c[7];
-	int p, k = 0, j,l=0,l1=-1;
+	int p, k = 0, j, l = 0, l1 = -1;
 	puts("Введите логин для поиска:");
 	scanf("%s", &c);
 	p = strlen(c);
-	printf("chisl = %d", chisl);
-	for (int n = 0;n < chisl;n++) {
-		for (int i = 0; i < chisl;i++) {
+	for (int n = 0; n < chisl; n++) {
+		for (int i = 0; i < chisl; i++) {
 			if (zap[n].login[i] == c[i]) {
 				k++;
-				printf("\n K = %d", k);
 			}
 			l = strlen(zap[n].login);
-			printf("\nL = %d P = %d", l,p);
 		}
 		if (l == p) {
-			if (((l == k ) || (l == k-1))) {
-				puts("Найденная структура:");
-				printf("\n|___________________________________________");
-				printf("\n|Логин пользователя(7сим):  %s\n", zap[n].login);
-				printf("|Дата выхода в on-line:  %d\n", zap[n].data_vh);
-				printf("|Время начала сеанса on-line:  %d:%d:%d\n", zap[n].online[0], zap[n].online[1], zap[n].online[2]);
-				printf("|Время завершения сеанса on-line:  %d:%d:%d\n", zap[n].ofline[0], zap[n].ofline[1], zap[n].ofline[2]);
-				printf("|скаченные/отправленные МБ:  %.2f/%.2f\t  \n", zap[n].dowloand, zap[n].send);
-				printf("|___________________________________________\n");
-				//l1++;
-				//poisk[l1] = n;
+			if (((l == k - 1))) {
+				return n;
+
+			}
+			else if ((l == k)) {
+				return n;
+			}
+			else {
+				(puts("НЕ НАЙДЕНО!"));
+				return -999;//проверка по поиску
 			}
 		}
 		k = 0;
 	}
-	printf("\n l1 = %d", l1);
-	return 0;
 }
 int poisk2(polz* zap, int chisl) {
 	int time[3], j;
 	printf("\nВведите длительность сеанса online(ч)");
 	scanf("%d", &j);
-	for (int chislo = 0;chislo < chisl;chislo++) {
-		time[0] = zap[chislo].ofline[0] - zap[chislo].online[0];
+	for (int chislo = 0; chislo < chisl; chislo++) {
+		time[0] = abs((zap[chislo].ofline[0] + zap[chislo].ofline[1] / 60 + zap[chislo].ofline[2] / 360) - (zap[chislo].online[0] + zap[chislo].online[1] / 60 + zap[chislo].online[2] / 360));
+		//printf("\nВРЕМЯ = %d", time[0]);
 		if (time[0] == j) {
 			puts("Найденная структура:");
-			printf("\n|___________________________________________");
-			printf("\n|Логин пользователя(7сим):  %s\n", zap[chislo].login);
-			printf("|Дата выхода в on-line:  %d\n", zap[chislo].data_vh);
-			printf("|Время начала сеанса on-line:  %d:%d:%d\n", zap[chislo].online[0], zap[chislo].online[1], zap[chislo].online[2]);
-			printf("|Время завершения сеанса on-line:  %d:%d:%d\n", zap[chislo].ofline[0], zap[chislo].ofline[1], zap[chislo].ofline[2]);
-			printf("|скаченные/отправленные МБ:  %.2f/%.2f\t  \n", zap[chislo].dowloand, zap[chislo].send);
-			printf("|___________________________________________\n");
-			//printf("%d)Сеанс длился с %d ч %d мин %d сек до %d ч %d мин %d сек ",chislo, zap[chislo].online[0], zap[chislo].online[1], zap[chislo].online[2], zap[chislo].ofline[0], zap[chislo].ofline[1], zap[chislo].ofline[2]);
+			return chislo;
+		}
+		else {
+			puts("НЕ НАЙДЕНО! ");
+			return -999;
 		}
 	}
-	return 0;
 }
 int sorting(polz* zap, int chislo) {
-	for (int i = 0;i < chislo;i++) {
+	for (int i = 0; i < chislo; i++) {
 		for (int j = 0; j < chislo - i - 1; j++) {
 			if (zap[j].dowloand + zap[j].send > zap[j + 1].dowloand + zap[j + 1].send) {
+				float n = zap[j].send+zap[j].dowloand;
+				float n1 = zap[j+1].send + zap[j + 1].dowloand;
 				float tmp1 = zap[j].dowloand;
 				float tmp2 = zap[j].send;
 				zap[j].dowloand = zap[j + 1].dowloand;
@@ -183,9 +176,9 @@ int readfile(polz* ptr_struct, int size, FILE* ptr_file) {
 		h++;
 		fscanf(ptr_file, "%s %d %d %d %d %d %d %d %f %f\n", &ptr_struct[h].login, &ptr_struct[h].data_vh, &ptr_struct[h].online[0], &ptr_struct[h].online[1], &ptr_struct[h].online[2], &ptr_struct[h].ofline[0], &ptr_struct[h].ofline[1], &ptr_struct[h].ofline[2], &ptr_struct[h].dowloand, &ptr_struct[h].send);
 	}
-	printf("Чтение данных с файла завершено %d\n", h);
+	printf("Чтение данных с файла завершено \n");
 	fclose(ptr_file);
-	if (size == 0) {return (h + size + 1);}
+	if (size == 0) { return (h + size + 1); }
 	if (size > 0) { return (h + size); }
 
 }
